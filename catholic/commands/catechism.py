76b58f33cd704f72ff0f1f9c9fb702efaf89900c
aseemsavio <aseemsavio3@gmail.com>
2022-10-01 12:47:01 +0530

    refactored, and comments added for catechism.

--- catholic/commands/catechism.py
+++ catholic/commands/catechism.py
@@ -1,30 +1,17 @@
 import click
 
-from catholic.core.catechism import get_catechism_by_paragraph
-from catholic.core.utils import load_pickle, show_green_block_text, show_error_message, show_markdown
+from catholic.core.catechism.catechism import execute_catechism_command
 
 
-@click.command()
+@click.command(help="- Query the Catechism of The Catholic Church.")
 @click.pass_context
 @click.option("--paragraph", "--p", type=int)
 def catechism(ctx: click.Context, paragraph):
     execute_catechism_command(paragraph)
 
 
-@click.command()
+@click.command(help="- Alias to Query the Catechism of The Catholic Church.")
 @click.pass_context
 @click.option("--paragraph", "--p", type=int)
 def c(ctx: click.Context, paragraph):
     execute_catechism_command(paragraph)
-
-
-def execute_catechism_command(paragraph):
-    catechism_dict = load_pickle("pickles/catechism.pickle")
-    if paragraph:
-        try:
-            catechism_para = get_catechism_by_paragraph(paragraph, catechism_dict)["text"]
-            # show_green_block_text(catechism_para)
-            show_markdown(catechism_para)
-        except IndexError as e:
-            error_message = f"🙁 The Catechism does not have a paragraph with ID: {paragraph}"
-            show_error_message(error_message)
--- catholic/core/catechism/catechism.py
+++ catholic/core/catechism/catechism.py
@@ -1,3 +1,22 @@
+from catholic.core.utils import load_pickle, show_markdown, show_error_message
+
+
+def execute_catechism_command(paragraph):
+    """
+    This function is a work-around to achieve command aliasing between "catechism" and "c".
+    :param paragraph: Paragraph_number
+    :return: None
+    """
+    catechism_dict = load_pickle("pickles/catechism.pickle")
+    if paragraph:
+        try:
+            catechism_para = get_catechism_by_paragraph(paragraph, catechism_dict)["text"]
+            show_markdown(catechism_para)
+        except IndexError as e:
+            error_message = f"🙁 The Catechism does not have a paragraph with ID: {paragraph}"
+            show_error_message(error_message)
+
+
 def get_catechism_by_paragraph(paragraph_number: int, catechism: dict):
     """
     Returns the contents of the paragraph in mark down format.
