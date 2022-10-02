8a746d4cd6073d898092dc263998e4656c08d0cb
aseemsavio <aseemsavio3@gmail.com>
2022-10-02 13:12:46 +0530

    implemented querying for canon law

--- catholic/core/canon/canon_law.py
+++ catholic/core/canon/canon_law.py
@@ -1,21 +1,31 @@
 from catholic.core.canon.services import get_canon_law_by_id
 from catholic.core.utils import load_pickle, show_error_message, show_markdown, show_blue_bold_block_text
+from catholic.core.utils.query import decode_query
 
 
 def execute_canon_command(law):
     canon_law_dict = load_pickle("pickles/canon.pickle")
     if law.isdigit():
-        try:
-            canon_law = get_canon_law_by_id(int(law), canon_law_dict)
-            if "text" in canon_law:
-                show_blue_bold_block_text(f"Canon Law: {canon_law['id']}")
-                show_markdown(canon_law["text"])
-            elif "sections" in canon_law:
-                for section in canon_law["sections"]:
-                    show_blue_bold_block_text(f"Canon Law: {canon_law['id']} :: §{section['id']}")
-                    show_markdown(section["text"])
-        except IndexError:
-            error_message = f"🙁 There is no Canon Law with ID: {law}"
-            show_error_message(error_message)
+        _display_canon_law(canon_law_dict, law)
     else:
-        print(law)
+        try:
+            for individual_law in decode_query(law):
+                _display_canon_law(canon_law_dict, individual_law)
+        except ValueError:
+            error_message = f"🙁 Could not decode the query: {law}"
+            show_error_message(error_message)
+
+
+def _display_canon_law(canon_law_dict, law):
+    try:
+        canon_law = get_canon_law_by_id(int(law), canon_law_dict)
+        if "text" in canon_law:
+            show_blue_bold_block_text(f"Canon Law: {canon_law['id']}")
+            show_markdown(canon_law["text"])
+        elif "sections" in canon_law:
+            for section in canon_law["sections"]:
+                show_blue_bold_block_text(f"Canon Law: {canon_law['id']} :: §{section['id']}")
+                show_markdown(section["text"])
+    except IndexError:
+        error_message = f"🙁 There is no Canon Law with ID: {law}"
+        show_error_message(error_message)
