2f2ed7263b73a85e7bb7c610dc2985a18cabdf63
aseemsavio <aseemsavio3@gmail.com>
2022-10-02 13:18:21 +0530

    implemented querying for catechism

--- catholic/core/catechism/catechism.py
+++ catholic/core/catechism/catechism.py
@@ -1,5 +1,6 @@
 from catholic.core.catechism.services import get_catechism_by_paragraph
 from catholic.core.utils import load_pickle, show_markdown, show_error_message, show_blue_bold_block_text
+from catholic.core.utils.query import decode_query
 
 
 def execute_catechism_command(paragraph):
@@ -10,12 +11,21 @@ def execute_catechism_command(paragraph):
     """
     catechism_dict = load_pickle("pickles/catechism.pickle")
     if paragraph.isdigit():
-        try:
-            catechism_para = get_catechism_by_paragraph(int(paragraph), catechism_dict)
-            show_blue_bold_block_text(f"Catechism Paragraph: {catechism_para['id']}")
-            show_markdown(catechism_para["text"])
-        except IndexError:
-            error_message = f"🙁 The Catechism does not have a paragraph with ID: {paragraph}"
-            show_error_message(error_message)
+        _display_catechism_paragraph(catechism_dict, paragraph)
     else:
-        print(paragraph)
+        try:
+            for individual_para in decode_query(paragraph):
+                _display_catechism_paragraph(catechism_dict, individual_para)
+        except ValueError:
+            error_message = f"🙁 Could not decode the query: {paragraph}"
+            show_error_message(error_message)
+
+
+def _display_catechism_paragraph(catechism_dict, paragraph):
+    try:
+        catechism_para = get_catechism_by_paragraph(int(paragraph), catechism_dict)
+        show_blue_bold_block_text(f"Catechism Paragraph: {catechism_para['id']}")
+        show_markdown(catechism_para["text"])
+    except IndexError:
+        error_message = f"🙁 The Catechism does not have a paragraph with ID: {paragraph}"
+        show_error_message(error_message)
