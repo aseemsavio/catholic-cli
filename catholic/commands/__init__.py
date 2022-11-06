83714714cbb868f1f01fdbd2a07e69d9ca7ac4b9
aseemsavio <aseemsavio3@gmail.com>
2022-11-06 08:08:19 +0530

    edited short forms for commands

--- catholic/commands/__init__.py
+++ catholic/commands/__init__.py
@@ -7,10 +7,10 @@ from catholic.commands.missal import missal
 
 def register_commands(group: click.Group):
     group.add_command(cmd=catechism, name="catechism")
-    group.add_command(cmd=catechism, name="c")
+    group.add_command(cmd=catechism, name="cm")
 
     group.add_command(cmd=canon, name="canon")
-    group.add_command(cmd=canon, name="cl")
+    group.add_command(cmd=canon, name="cn")
 
     group.add_command(cmd=missal, name="missal")
-    group.add_command(cmd=missal, name="m")
+    group.add_command(cmd=missal, name="ml")
