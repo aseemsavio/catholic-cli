6b5bd9cffbe9c38dc800a41eece1df33c1596616
aseemsavio <aseemsavio3@gmail.com>
2022-12-10 13:03:28 +0530

    callback to execute only at startup

--- catholic/catholic.py
+++ catholic/catholic.py
@@ -1,10 +1,19 @@
 from catholic.commands import register_commands
 from catholic.version import version
-from typer import Typer
+from typer import Typer, Context
 
 __version__ = version
 
 # This is the root object to which all the commands get registered to.
 cli = Typer()
+
+
+@cli.callback(invoke_without_command=True)
+def catholic_features_callback(ctx: Context):
+    # Execute this only if there is no sub command.
+    # https://typer.tiangolo.com/tutorial/commands/context/#exclusive-executable-callback
+    if ctx.invoked_subcommand is None:
+        print(f"callback reached {ctx.obj}")
+
 
 register_commands(cli)
